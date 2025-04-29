ba195637fdfba98f4363d5500d820773951f2968
RedCrafter07 <gh@r07.dev>
2025-04-29 19:04:46 +0200

    feat(ssr): Add ssr handler for dev and prod

---
+++ components/router/ssrRoute.ts
@@ -0,0 +1,26 @@
+import type { ViteDevServer } from 'vite';
+
+export default async function ssrRoute(
+	mode: 'dev' | 'prod',
+	path: string,
+	htmlPath: string,
+	modulePath: string,
+	devServer?: ViteDevServer,
+) {
+	if (mode === 'dev' && devServer) {
+		const viteHtml = await devServer.transformIndexHtml(
+			path,
+			await Bun.file(htmlPath).text(),
+		);
+
+		const { default: render } = (await devServer.ssrLoadModule(modulePath)) as {
+			default: (path: string, template: string) => Promise<404 | string>;
+		};
+
+		return await render(path, viteHtml);
+	} else {
+		const { default: render } = await import(modulePath);
+
+		return await render(path, await Bun.file(htmlPath).text());
+	}
+}
